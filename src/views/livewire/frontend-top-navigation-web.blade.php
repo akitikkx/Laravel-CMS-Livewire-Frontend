bea0615741904c0a0bc7e24eb91e964e57435bcc
Ahmed Tikiwa <ahmed@aewebmasters.co.za>
2021-03-17 19:38:50 +0200

    Updated view foreach for navigation links

--- src/views/livewire/frontend-top-navigation-web.blade.php
+++ src/views/livewire/frontend-top-navigation-web.blade.php
@@ -1,5 +1,5 @@
 <div class="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
-    @foreach ($topNavLinks as $link)
+    @foreach ($topNavLinks as $item)
         <a href=href="{{ url('/'.$item->slug) }}"
            class="font-medium text-gray-500 hover:text-gray-900">{{ $item->label }}</a>
     @endforeach
