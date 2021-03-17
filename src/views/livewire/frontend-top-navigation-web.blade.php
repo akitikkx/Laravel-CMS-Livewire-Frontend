8f8c7b7c6bf44dbc7f17e1cf13b8799d86ebc9f7
Ahmed Tikiwa <ahmed@aewebmasters.co.za>
2021-03-17 19:47:56 +0200

    Fixed href for top and mobile links

--- src/views/livewire/frontend-top-navigation-web.blade.php
+++ src/views/livewire/frontend-top-navigation-web.blade.php
@@ -1,6 +1,6 @@
 <div class="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
     @foreach ($topNavLinks as $item)
-        <a href=href="{{ url('/'.$item->slug) }}"
+        <a href="{{ url('/'.$item->slug) }}"
            class="font-medium text-gray-500 hover:text-gray-900">{{ $item->label }}</a>
     @endforeach
 </div>
